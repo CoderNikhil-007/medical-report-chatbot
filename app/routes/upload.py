36ecf272a626fbfd2a0b769b4040c4d88e0bd072
Nikhil Mishra <nmdp1301@gmail.com>
2026-04-07 14:53:23 +0530

    update fix

--- app/routes/upload.py
+++ app/routes/upload.py
@@ -1,13 +1,14 @@
 from fastapi import APIRouter, UploadFile
 import os
+from app.services.pdf_loader import extract_text_with_metadata
+from app.services.embeddings import chunk_documents, get_embeddings
+from app.services.vector_store import create_index
 
 router = APIRouter()
 
 @router.post("/upload")
 async def upload_file(file: UploadFile):
-    from app.services.pdf_loader import extract_text_with_metadata
-    from app.services.embeddings import chunk_documents, get_embeddings
-    from app.services.vector_store import create_index
+    
     file_path = f"data/{file.filename}"
     
     with open(file_path, "wb") as f:
