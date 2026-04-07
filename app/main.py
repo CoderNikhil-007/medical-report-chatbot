4d48389d94b21098360da498306d998f0b5fb6b6
Nikhil Mishra <nmdp1301@gmail.com>
2026-04-07 13:34:39 +0530

    test code

--- app/main.py
+++ app/main.py
@@ -1,12 +1,20 @@
-from fastapi import FastAPI
-from app.routes import upload, query
-import warnings
+# from fastapi import FastAPI
+# from app.routes import upload, query
+# import warnings
 
-# Ignore only specific warnings
-warnings.filterwarnings("ignore", category=UserWarning)
-warnings.filterwarnings("ignore", category=FutureWarning)
+# # Ignore only specific warnings
+# warnings.filterwarnings("ignore", category=UserWarning)
+# warnings.filterwarnings("ignore", category=FutureWarning)
+
+# app = FastAPI()
+
+# app.include_router(upload.router)
+# app.include_router(query.router)
+
+from fastapi import FastAPI
 
 app = FastAPI()
 
-app.include_router(upload.router)
-app.include_router(query.router)
+@app.get("/")
+def root():
+    return {"message": "API is working"}
